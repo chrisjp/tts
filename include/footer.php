372758fae173f34ce1ee9c899b22d1267c9b1146
Chris Phillips <chrisjp88@gmail.com>
2023-06-29 17:26:44 +0100

    header/footer includes
    No point using a full templating system. This'll do.

---
+++ include/footer.php
@@ -0,0 +1,31 @@
+<?php
+// Don't access this file directly
+if (__FILE__ === $_SERVER['SCRIPT_FILENAME']) {
+    header('Location: ../');
+    exit;
+}
+?>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p class="is-size-7-touch">
+                TTS Demo by <a href="https://chrisphillips.uk">Chris Phillips</a><br />
+                Powered by: <a href="https://aws.amazon.com/polly/">Amazon Polly</a> (via <a href="https://streamlabs.com/">Streamlabs</a> API)
+                // <a href="https://cereproc.com/">CereProc</a>
+                // <a href="https://tiktok.com">TikTok</a>
+                // <a href="https://www.ibm.com/cloud/watson-text-to-speech/">IBM Watson</a>
+                // <a href="https://www.acapela-group.com/demos/">Acapela</a>
+                // <a href="https://www.oddcast.com/ttsdemo/index.php">Oddcast</a>
+                // <a href="https://translate.google.com/">Google Translate</a>
+            </p>
+            <p>
+                <a href="https://github.com/chrisjp/tts"><img src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg" width="32" height="32" alt="GitHub" /></a>
+            </p>
+        </div>
+    </footer>
+    <!-- JS -->
+    <script src="assets/js/tts.js?v=<?php echo $tts::version; ?>"></script>
+<?php if (basename($_SERVER['SCRIPT_FILENAME']) === "conversation.php") echo '    <script src="assets/js/conversations.js?v=' . $tts::version . '"></script>' . PHP_EOL; ?>
+    </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,42 @@
+<?php
+// Don't access this file directly
+if (__FILE__ === $_SERVER['SCRIPT_FILENAME']) {
+    header('Location: ../');
+    exit;
+}
+
+require $_SERVER['DOCUMENT_ROOT']. '/vendor/autoload.php';
+use ChrisJP\TTS\TTS;
+$tts = new TTS();
+
+$thisPage = basename($_SERVER['SCRIPT_FILENAME']);
+$tabHighlight = 'is-active has-text-weight-bold';
+?>
+<!doctype html>
+<html lang="en" class="mode-light has-background-white-bis">
+    <head>
+        <meta charset="utf-8">
+        <title>Text-to-Speech (TTS) StreamElements Demo Simulator Emulator Generator Tool Thing</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css" integrity="sha512-HqxHUkJM0SYcbvxUw5P60SzdOTy/QVwA1JJrvaXJv4q7lmbDZCmZaqz01UPOaQveoxfYRv1tHozWGPMcuTBuvQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="assets/css/tts.css?v=<?php echo $tts::version; ?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    </head>
+    <body>
+    <section class="section">
+        <div class="container">
+            <div id="toggleStyleMode" class="is-pulled-right toggle-style" onclick="toggleStyleMode(true);">💡</div>
+            <h1 class="title is-2">Text-to-Speech Simulator</h1>
+            <p class="subtitle is-4">A simple web app demonstrating how text sounds in different TTS voices.</p>
+
+            <div id="tab-container">
+                <div class="tabs is-centered">
+                    <ul>
+                    <li class="<?php if ($thisPage === 'index.php') echo $tabHighlight; ?>"><a href="./">Demo</a></li>
+                    <li class="<?php if ($thisPage === 'conversation.php' || $thisPage === 'playlist.php') echo $tabHighlight; ?>"><a href="./conversation.php">Conversation</a></li>
+                    </ul>
+                </div>
+            </div>
+
